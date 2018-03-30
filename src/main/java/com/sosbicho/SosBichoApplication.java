b7a53194efc20f53d8aedf0fe98d94ad1f052339
Felipe Kunzler <felipe.kunzler@sap.com>
2018-03-30 09:13:35 -0300

    Create spring boot project

---
+++ src/main/java/com/sosbicho/SosBichoApplication.java
@@ -0,0 +1,13 @@
+package com.sosbicho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosBichoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosBichoApplication.class, args);
+	}
+
+}
