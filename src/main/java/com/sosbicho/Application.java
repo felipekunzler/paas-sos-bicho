c630ac2bbe35cd5e80e6c3d023a103d2577ab39a
Felipe Kunzler <felipe.kunzler@sap.com>
2018-03-31 11:22:19 -0300

    Create login page

--- src/main/java/com/sosbicho/Application.java
+++ src/main/java/com/sosbicho/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SosBichoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SosBichoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/main/java/com/sosbicho/controller/HomeController.java
+++ src/main/java/com/sosbicho/controller/HomeController.java
@@ -1,23 +1,19 @@
 package com.sosbicho.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/")
-    public String index() {
+    @GetMapping("/")
+    public String home() {
         return "home";
     }
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "home";
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 
 }
