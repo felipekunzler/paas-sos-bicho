bb4bbe1d6641b922f4fe9eddc1a3703c0eca0ab4
Felipe Kunzler <felipe.kunzler@sap.com>
2018-06-22 17:13:31 -0300

    Fix bug

--- src/main/java/com/sosbicho/service/JpaUserDetailsService.java
+++ src/main/java/com/sosbicho/service/JpaUserDetailsService.java
@@ -19,7 +19,7 @@ public class JpaUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
         com.sosbicho.domain.User user = repository.findByUsernameIgnoreCase(name);
         if (user == null) {
-            return null;
+            throw new UsernameNotFoundException("User not found");
         }
         return new User(user.getUsername(), user.getPassword(), AuthorityUtils.createAuthorityList("ROLE_USER"));
     }
